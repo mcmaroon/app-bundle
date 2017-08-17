3ba420c25f961736f08dead8f42619b61c221492
Marek Ciarkowski <marek.ciarkowski@gmail.com>
2017-08-17 14:36:00 +0200

    ClearInputTrait clearString fix

--- Helper/Traits/ClearInputTrait.php
+++ Helper/Traits/ClearInputTrait.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 trait ClearInputTrait {
 
     public function clearString($string) {
-        return trim(filter_var($string, FILTER_SANITIZE_STRING, FILTER_SANITIZE_SPECIAL_CHARS));
+        return trim(filter_var($string, FILTER_UNSAFE_RAW));
     }
 
     public function clearNumber($string) {
